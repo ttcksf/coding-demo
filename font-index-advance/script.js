5cb0e896e2bec0b0d5da341f6126b234b188b219
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-13 01:34:19 +0900

    font-index-advance

---
+++ font-index-advance/script.js
@@ -0,0 +1,12 @@
+const text = 'Code Tips';
+
+// 最初に登場するインデックス番号を返す（0以上であればある）
+// console.log(text.indexOf('C'));
+// 最後に登場するインデックス番号を返す（0以上であればある）
+// console.log(text.lastIndexOf('C'));
+// 含まれるかを返す（trueであればある）
+// console.log(text.includes('C'));
+// 引数に渡したインデックス番号に何があるか返す（位置が推測できる時のみ）
+// console.log(text.charAt(0));
+// 引数に渡した文字から始まるか返す（位置が推測できる時のみ）
+console.log(text.startsWith('C'));
