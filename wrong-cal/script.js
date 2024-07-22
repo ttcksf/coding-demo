3af33d51cf647e6762ce106e4372f44128b8a173
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-22 14:58:53 +0900

    wrong-cal

---
+++ wrong-cal/script.js
@@ -0,0 +1,24 @@
+let number = 0;
+
+function addNum() {
+  // number += 1;
+  number += 0.1;
+  console.log(number);
+  // if (number === 1) {
+  // if (number >= 1) {
+  if (Math.floor(number) === 1) {
+    console.log('1になった');
+  }
+}
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
+addNum();
