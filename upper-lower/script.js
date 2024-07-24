841a7a320697f020f4b3908028f257d8aa5548ef
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-24 20:42:45 +0900

    upper-lower

---
+++ upper-lower/script.js
@@ -0,0 +1,7 @@
+let text = 'AAA';
+text = text.toLowerCase();
+console.log(text); // aaaと出力される
+text = text.toUpperCase();
+console.log(text); // AAAと出力される
+title = title[0].toUpperCase() + title.slice(1).toLowerCase();
+console.log(text); // Aaaと出力される
