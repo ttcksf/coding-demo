d1ff9267c125b51351c0dc6dbcb476da41f5882f
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-08-16 18:12:20 +0900

    intersection-observer

---
+++ intersection-observer/script.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll('.card');
+
+// 1.監視している要素の表示状態が変化するたびに呼び出される
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // 監視対象が画面に見えたら(isIntersecting)、activeを追加する
+    entry.target.classList.toggle('active', entry.isIntersecting);
+  });
+  // 2.
+  // console.log(entries);
+});
+// 2.observeは監視対象を監視するメソッド（1つ目にactiveを追加するとコールバック関数内のconsole.logが実行される）
+// observer.observe(cards[0]);
+
+// 3.card全てを監視対象に追加するとアニメーションになる
+cards.forEach((card) => {
+  observer.observe(card);
+});
